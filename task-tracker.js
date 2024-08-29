2cb6b9d23d58b7176538ec48c510ae30fb0d6984
Himanshu Pareek <himanshu@indihood.com>
2024-08-29 19:32:23 +0530

    Adds update and delete features

--- task-tracker.js
+++ task-tracker.js
@@ -1,30 +1,114 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const FILE_NAME = "tasks.json";
+const FILE_NAME = "tasks.json"
 
-main(process.argv.splice(2));
+main(process.argv.splice(2))
 
 function handleAddCommand(args = []) {
     if (args.length < 2) {
-        console.error('ERROR: Please specify the task description');
-        console.error('Ex. task-cli add "Buy Groceries"');
-        return;
+        console.error('ERROR: Please specify the task description')
+        console.error('Ex. task-cli add "Buy Groceries"')
+        process.exit(1)
     }
-    const taskDescription = args[1].trim();
-    if (taskDescription.length == 0) {
-        throw new Error("Task description must not be empty");
+    const taskDescription = args[1].trim()
+    validateTaskDescription(taskDescription)
+    const createdTask = createTask(taskDescription)
+    console.log(`Task added successfully (ID: ${createdTask.id})`)
+}
+
+function handleUpdateCommand(args = []) {
+    if (args.length < 3) {
+        console.error('ERROR: Please specify the task id and updated task description')
+        console.error('Ex. task-cli update 1 "Cook dinner"')
+        process.exit(1)
     }
-    const createdTask = createTask(taskDescription);
-    console.log(`Task added successfully (ID: ${createdTask.id})`);
+    const id = parseInt(args[1].trim())
+    const description = args[2].trim()
+    validateTaskId(id)
+    validateTaskDescription(description)
+    try {
+        const oldDescription = updateTask(id, description)
+        console.log(`Task (ID: ${id}) updated successfully.`)
+        console.log(`${oldDescription} -> ${description}`)
+    } catch (e) {
+        console.error(e.toString())
+    }
+}
+
+function handleDeleteCommand(args = []) {
+    if (args.length < 2) {
+        console.error('ERROR: Please specify the task id to delete')
+        console.error('Ex. task-cli delete 1')
+        process.exit(1)
+    }
+    const id = parseInt(args[1].trim())
+    validateTaskId(id)
+    try {
+        const task = deleteTask(id)
+        console.log(`Task "${task.description}" (id = ${id}, status = ${task.status}) deleted successfully.`)
+    } catch (e) {
+        console.error(e.toString())
+    }
+}
+
+function validateTaskDescription(description) {
+    if (description.length == 0) {
+        console.error('ERROR: Task description must not be empty');
+        process.exit(1);
+    }
+}
+
+function validateTaskId(id) {
+    if (isNaN(id)) {
+        console.error('ERROR: Task id must be an integer');
+        process.exit(1);
+    }
 }
 
 function createTask(description = "") {
-    let tasks = getAllTasks();
+    const tasks = getAllTasks();
     const task = taskFromIdAndDescription(tasks.nextId, description);
     tasks.items.push(task);
     tasks.nextId += 1;
     saveAllTasks(tasks);
     return task;
+}
+
+function updateTask(id, description) {
+    const tasks = getAllTasks()
+    const task = getMatchingTask(tasks, id)
+    if (!task) {
+        throw new Error(`Task with id = ${id} does not exist`)
+    }
+    const oldDescription = task.description
+    task.description = description
+    task.updatedAt = new Date()
+    saveAllTasks(tasks)
+    return oldDescription
+}
+
+function deleteTask(id) {
+    const tasks = getAllTasks()
+    const task = getMatchingTask(tasks, id)
+    if (!task) {
+        throw new Error(`Task with id = ${id} does not exist`);
+    }
+    removeMatchingTask(tasks, id)
+    saveAllTasks(tasks)
+    return task
+}
+
+function getMatchingTask(tasks, id) {
+    for (let task of tasks.items) {
+        if (task.id === id) {
+            return task
+        }
+    }
+    return null
+}
+
+function removeMatchingTask(tasks, id) {
+    tasks.items = tasks.items.filter(task => task.id !== id)
 }
 
 function getAllTasks() {
@@ -60,6 +144,10 @@ function main(args = []) {
     const command = args[0].trim()
     if (command === 'add') {
         handleAddCommand(args);
+    } else if (command === 'update') {
+        handleUpdateCommand(args);
+    } else if (command === 'delete') {
+        handleDeleteCommand(args)
     }
 }
 
