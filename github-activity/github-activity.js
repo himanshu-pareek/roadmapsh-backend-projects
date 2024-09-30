92ea2862e4a0adc1f34f15c45164c3dbcbb5898e
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-09-30 22:40:59 +0530

    Handles invalid username

--- github-activity/github-activity.js
+++ github-activity/github-activity.js
@@ -5,7 +5,11 @@ const ACTIVITY_URL = 'https://api.github.com/users/{USERNAME}/events'
 async function handleUsername (username = '') {
     console.log(`Getting github activity for ${username} ...`)
     const activityUrl = getActivityUrl(username)
-    const activities = await getActivities(activityUrl)
+    try {
+        const activities = await getActivities(activityUrl)
+    } catch (e) {
+        console.error(e.toString())
+    }
 }
 
 function getActivityUrl(username = '') {
@@ -15,23 +19,27 @@ function getActivityUrl(username = '') {
 async function getActivities(activityUrl = '') {
     console.log(`Sending request to ${activityUrl} ...`)
     const res = await fetch(activityUrl)
+    if (res.status == 404) {
+        throw new Error('User not found. Try with another username.')
+    }
     const json = await res.json()
     console.log('Got response. Parsing response...')
     fs.writeFileSync("actitities.json", JSON.stringify(json, undefined, 4))
 }
 
 function main(args = []) {
-    if (args.length != 1) {
-        usage()
-    }
-    if (!args[0]) {
-        usage()
-    }
-    const username = args[0].trim()
-    if (username.length == 0) {
-        usage()
-    }
-    handleUsername(username)
+    // if (args.length != 1) {
+    //     usage()
+    // }
+    // if (!args[0]) {
+    //     usage()
+    // }
+    // const username = args[0].trim()
+    // if (username.length == 0) {
+    //     usage()
+    // }
+    // handleUsername(username)
+    handleUsername('lsmoosn')
 }
 
 function usage() {
