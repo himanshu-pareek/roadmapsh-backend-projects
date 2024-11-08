4363e4173f46f91a29133d8e5a0f7f298c90ffe9
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-11-08 19:04:08 +0530

    Implements deletion of expense using id

--- expense-tracker/expense-tracker.sh
+++ expense-tracker/expense-tracker.sh
@@ -115,11 +115,60 @@ add() {
   # 5.5 Store the total spending in summary file
   echo "${total_spending}" > "${summary_file}"
 
-  echo "Added expense with ID - ${id}"
+  echo "✅  Added expense with ID - ${id}"
 }
 
 delete() {
-  echo "Delete"
+  id="-"
+  idProvided=0
+  while [[ $# > 0 ]]; do
+    case "$1" in
+      "--id")
+        if [[ $# < 2 ]]; then
+          usage
+        fi
+        id="$2"
+        idProvided=1
+        shift
+        shift
+        ;;
+      *)
+        echo "Unrecognized argument: $1"
+        usage
+    esac
+  done
+  echo "Deleting expense with id - ${id}"
+  expense_file="${EXPENSES_DIR}/${id}.expense"
+  # 1. Check if the expense file exists
+  if [[ ! -f "${expense_file}" ]]; then
+    echo "No expense found with id ${id}"
+    exit 0
+  fi
+
+  # 2. Read the file to get data
+  expense_date=`head -n 1 "${expense_file}"`
+  category=`head -n 2 "${expense_file}" | tail -n 1`
+  amount=`head -n 4 "${expense_file}" | tail -n 1`
+
+  # 3. Calculate the month from expense_date
+  expense_month=`dateToYearMonth "${expense_date}"`
+
+  # 4. Reduce the spending for category by expense's amount
+  # 4.1 Check if file exists
+  summary_file="${SUMMARIES_DIR}/${expense_month}/${category}.spending"
+  if [[ -f "${summary_file}" ]]; then
+    # 4.2 Read the previoud spending from the file
+    previous_spending=`cat "${summary_file}"`
+    # 4.3 Calculate new spending
+    remaining_spending=$(echo "${previous_spending}-${amount}" | bc)
+    # 4.4 Write remaining spending back to the summary file
+    echo "${remaining_spending}" > "${summary_file}"
+  fi
+
+  # 5. Delete the expense file
+  rm "${expense_file}"
+
+  echo "🗑️  Deleted expense with id ${id}"
 }
 
 summary() {
@@ -128,6 +177,12 @@ summary() {
 
 budget() {
   echo "Budget"
+}
+
+dateToYearMonth() {
+  year=`echo $1 | cut -d "-" -f 1`
+  month=`echo $1 | cut -d "-" -f 2`
+  echo "$year-$month"
 }
 
 "$@"
