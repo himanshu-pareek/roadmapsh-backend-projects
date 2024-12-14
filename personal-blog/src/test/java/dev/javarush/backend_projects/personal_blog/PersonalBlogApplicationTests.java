9217a167ddc030315b710eb4fa26462c64fbe9c3
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-12-14 21:11:46 +0530

    Sets up project

---
+++ personal-blog/src/test/java/dev/javarush/backend_projects/personal_blog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javarush.backend_projects.personal_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
