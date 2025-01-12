275fc8d06e9624e40245c1f07b1fb3575bcdc0f0
Himanshu Pareek <himanshupareekiit01@gmail.com>
2025-01-12 12:57:10 +0530

    Creates new article

--- personal-blog/src/main/java/dev/javarush/backend_projects/personal_blog/article/web/ArticleController.java
+++ personal-blog/src/main/java/dev/javarush/backend_projects/personal_blog/article/web/ArticleController.java
@@ -1,17 +1,18 @@
 package dev.javarush.backend_projects.personal_blog.article.web;
 
-import java.util.UUID;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import dev.javarush.backend_projects.personal_blog.article.Article;
 import dev.javarush.backend_projects.personal_blog.article.ArticleId;
 import dev.javarush.backend_projects.personal_blog.article.ArticleService;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -42,6 +43,27 @@ public class ArticleController {
         var articles = this.articleService.getAllArticles();
         model.addAttribute("articles", articles);
         return "articles/admin";
+    }
+
+    @GetMapping("new")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String createArticleForm(Model model) {
+        model.addAttribute("editMode", EditMode.CREATE);
+        model.addAttribute("article", new CreateArticleFormData());
+        return "articles/edit";
+    }
+
+    @PostMapping("articles")
+    public String createArticle(
+            @Validated @ModelAttribute("article") CreateArticleFormData data,
+            BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("editMode", EditMode.CREATE);
+            return "articles/edit";
+        }
+        this.articleService.createArticle(data.toParameters());
+        return "redirect:/admin";
     }
 
 }
---
+++ personal-blog/src/main/java/dev/javarush/backend_projects/personal_blog/article/web/EditMode.java
@@ -0,0 +1,6 @@
+package dev.javarush.backend_projects.personal_blog.article.web;
+
+public enum EditMode {
+    CREATE,
+    UPDATE
+}
