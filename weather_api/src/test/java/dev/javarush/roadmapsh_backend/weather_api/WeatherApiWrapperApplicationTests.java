ef677282b505335a5a54c06c3b60794c9ac967ae
Himanshu Pareek <himanshupareekiit01@gmail.com>
2025-02-08 15:04:35 +0530

    Initializes project for weather api wrapper

---
+++ weather_api/src/test/java/dev/javarush/roadmapsh_backend/weather_api/WeatherApiWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javarush.roadmapsh_backend.weather_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
