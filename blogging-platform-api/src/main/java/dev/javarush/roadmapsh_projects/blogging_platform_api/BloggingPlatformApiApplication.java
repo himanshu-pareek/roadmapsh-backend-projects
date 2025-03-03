aa8fce60a9742b6b0b0d1fea6c322c0b9afc8369
Himanshu Pareek <himanshupareekiit01@gmail.com>
2025-03-03 22:08:33 +0530

    Initializes project for Blogging Platform APIs

---
+++ blogging-platform-api/src/main/java/dev/javarush/roadmapsh_projects/blogging_platform_api/BloggingPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package dev.javarush.roadmapsh_projects.blogging_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingPlatformApiApplication.class, args);
+	}
+
+}
---
+++ blogging-platform-api/src/test/java/dev/javarush/roadmapsh_projects/blogging_platform_api/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javarush.roadmapsh_projects.blogging_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
